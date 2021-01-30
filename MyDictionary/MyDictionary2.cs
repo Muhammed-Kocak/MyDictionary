76fc40009df2a734adf74cae2c393d34111ede3d
MUHAMMED KOÇAK <muhammed_kocak26@hotmail.com>
2021-01-30 23:53:02 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary2<TKEY,TVALUE> //GÖNDERDİĞİMİZ TÜRE GÖRE GENERİC TİPTE BİR DİCTİONARY OLUŞTURUYORUZ.
+    {
+        TKEY[] keys; //T TÜRÜNDE KEY VE VALUE ARRAY TANIMLIYORUZ.
+        TVALUE[] values;
+
+        public MyDictionary2()
+        {
+            keys = new TKEY[0]; // CONSTRUCTOR'DA ARRAYLERİMİZDE KAÇ ELEMANLI OLACAĞINI BELİRLİYORUZ.
+            values = new TVALUE[0];
+        }
+        public void Add(TKEY key , TVALUE value) //ADD METOTUMUZDAN T TÜRÜNDE KEY VE VALUE ALIYORUZ KULLANICIDAN
+        {
+            TKEY[] tempKeys = keys; //T TÜRÜNDE DAHA ÖNCEDEN OLUŞTURDUĞUMUZ BİR ARRAY VAR İSE ONUN İÇİNDEKİ BİLGİLERİN 
+            TVALUE[] tempValue = values; //ADRESİNİ TUTACAK BİR TEMP ARRAY OLUŞTURUYORUZ VE ONLARIN ADRESLERİNİ EŞLİYORUZ.
+
+            keys = new TKEY[keys.Length + 1]; //KEY VEYA VALUE'NİN ELEMAN SAYISINDAN BİR FAZLASI KADARINI TEKRAR ARRAY OLUŞTURUYORUZ.
+            values = new TVALUE[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++) // DÖNGÜ İLE TEMP'E AKTARDIĞIMIZ BİLGİLERİ TEKRARDAN YENİ OLUŞTURDUĞUMUZ DİZİYE AKTARIYORUZ.
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key; //KULLANICIDAN ALDIĞIMIZ KEY VE VALUE'Yİ ARRAY'İMİZİN EN SON İNDEXİNE YERLEŞTİRİYORUZ.
+            values[values.Length - 1] = value;
+        }
+        public TKEY[] Keys 
+        {
+            get { return keys; }
+        }
+
+        public TVALUE[] Values
+        {
+            get { return values; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary2<int, string> myDictionary2 = new MyDictionary2<int, string>();
+            myDictionary2.Add(1, "Hello");
+            myDictionary2.Add(2, "Word");
+            Console.WriteLine(myDictionary2.Keys[0]+" : " + myDictionary2.Values[0]);
+            Console.WriteLine(myDictionary2.Keys[1]+" : " + myDictionary2.Values[1]);
+
+            MyDictionary2<string, string> myDictionary3 = new MyDictionary2<string, string>();
+            myDictionary3.Add("Meyve", "Elma");
+            myDictionary3.Add("Sebze", "Ispanak");
+            Console.WriteLine(myDictionary3.Keys[0] + " : " + myDictionary3.Values[0]);
+            Console.WriteLine(myDictionary3.Keys[1] + " : " + myDictionary3.Values[1]);
+
+        }
+    }
+}
